2ef8dfe667133128f30a0416254bcf9234442088
HỮU NHÃ NGUYỄN <nguyenhuunha@gmail.com>
2022-02-14 13:44:50 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,13 @@
+import db from "../models/index";
 
 let getHomePage = (req, res) => {
+
+    try {
+        let data = await db.User.findAll();
+    } catch (error) {
+        
+    }
+    
     return res.render('homepage.ejs');
 }
 
